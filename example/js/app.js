da9d31728e38bc779f21edee0503e59e63f0fbc9
Roland Zwaga <roland@stackandheap.com>
2014-06-23 18:43:30 +0200

    Using the scope in trait method

--- example/js/app.js
+++ example/js/app.js
@@ -3,12 +3,14 @@ var app = angular.module('demo', ['angular-mixin']);
 app.trait('testtrait', {
     mixinMessage: 'This is my mixed in message',
     someMethod:function() {
-        alert(this.myMessage);
+        alert(this.scope.myMessage);
     }
 });
 
 app.controller('testController', ['$scope', function($scope) {
     this.myMessage = "This is my own message";
+    this.scope = $scope;
+    this.scope.myMessage = this.myMessage;
     $scope.message = this.mixinMessage;
     $scope.onClick = this.someMethod.bind(this);
 }]).mixin('testtrait');

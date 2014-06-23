1076d36b5442102c9f31b1e00e5ed889bb3fc7ef
Roland Zwaga <roland@stackandheap.com>
2014-06-23 18:28:47 +0200

    Added a mixin method to the example

--- example/js/app.js
+++ example/js/app.js
@@ -4,9 +4,14 @@
 var app = angular.module('demo', ['angular-mixin']);
 
 app.trait('testtrait', {
-    mixinMessage: 'This is my mixed in message'
+    mixinMessage: 'This is my mixed in message',
+    someMethod:function() {
+        alert(this.myMessage);
+    }
 });
 
 app.controller('testController', ['$scope', function($scope) {
+    this.myMessage = "This is my own message";
     $scope.message = this.mixinMessage;
+    $scope.onClick = this.someMethod.bind(this);
 }]).mixin('testtrait');
